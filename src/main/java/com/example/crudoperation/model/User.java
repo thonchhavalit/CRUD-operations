3d75f00d8a30beccd2d8949faff7ce635575a4af
thonchhavalit <valitgamming@gmail.com>
2023-05-08 23:19:04 +0700

    fixed: up to date

--- src/main/java/com/example/crudoperation/model/User.java
+++ src/main/java/com/example/crudoperation/model/User.java
@@ -3,10 +3,12 @@ package com.example.crudoperation.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Accessors(chain = true)
 public class User {
     private int UserID;
     private String username;
